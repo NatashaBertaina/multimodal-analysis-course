1984c906d73d4ef0a92420342861cc10ba588c4c
Johanna Casado <johi.ceh@gmail.com>
2024-04-04 19:07:27 -0300

    The plot with braille labels using the pybrl library was added.

--- sound_noise.py
+++ sound_noise.py
@@ -7,6 +7,7 @@ Created on Wed Jan 24 08:30:39 2024
 
 #General import
 import os
+import sys
 import argparse
 import glob
 import numpy as np
@@ -16,6 +17,10 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import math
 import pandas as pd
+
+sys.path.append("../pybrl")
+
+import pybrl as brl
 
 from pydub import AudioSegment
 
@@ -93,23 +98,121 @@ data, status, msg = _dataimport.set_arrayfromfile(path, ext)
 if data.shape[1]<2:
     print("Error reading file 1, only detect one column.")
     exit()
-
 # Extract the names and turn to float
 data_float = data.iloc[1:, :].astype(float)
 x_pos_min = 1
 
-# Generate the plot if needed
+# Generate de plot
 ax.set_xlabel('x')
-ax.set_ylabel('y')
+ax.set_ylabel('y', rotation=0)
 # Separate the name file from the path to set the plot title
 filename = os.path.basename(path)
-#Plot 
+# Plot 
 ax.plot(data_float.loc[:, 0], data_float.loc[:, 1], '#2874a6', linewidth=3)
-ax.axhline(y=0, color='k', linewidth=1)
-ax.axvline(x=0, color='k', linewidth=1)
+# Ejes de coordenadas
+if data_float.loc[:, 0].min() < 0:
+    ax.axvline(x=0, color='k', linewidth=1)
+if data_float.loc[:, 1].min() < 0:
+    ax.axhline(y=0, color='k', linewidth=1)
 # Set the path to save the plot and save it
 plot_path = path[:-4] + 'plot.png'
 fig.savefig(plot_path)
+plt.close()
+
+# Generate the braille plot
+figbraille = plt.figure()
+axbraille = plt.axes()
+
+# 3 valores de eje x en braille
+
+abs_val_array = np.abs(data_float.loc[:,0] - data_float.loc[:,0].min())
+x_pos_min = abs_val_array.idxmin()
+middle = ((data_float.loc[:,0].max() - data_float.loc[:,0].min())/2) + data_float.loc[:,0].min()
+abs_val_array = np.abs(data_float.loc[:,0] - middle)
+x_pos_middle = abs_val_array.idxmin()
+abs_val_array = np.abs(data_float.loc[:,0] - data_float.loc[:,0].max())
+x_pos_max = abs_val_array.idxmin()
+
+xinicio_text = str(int(data_float.loc[x_pos_min,0]))
+xinicio_text = brl.translate(xinicio_text)
+if data_float.loc[x_pos_min,0] < 0:
+    caract_resta = [['001001']]
+    for i in xinicio_text[0]:
+        caract_resta[0].append(i)
+    xinicio_text = caract_resta
+xinicio_text = brl.toUnicodeSymbols(xinicio_text, flatten=True)
+xmedio_text = str(int(data_float.loc[x_pos_middle,0]))
+xmedio_text = brl.translate(xmedio_text)
+if data_float.loc[x_pos_middle,0] < 0:
+    caract_resta = [['001001']]
+    for i in xmedio_text[0]:
+        caract_resta[0].append(i)
+    xmedio_text = caract_resta
+xmedio_text = brl.toUnicodeSymbols(xmedio_text, flatten=True)
+xfinal_text = str(int(data_float.loc[x_pos_max,0]))
+xfinal_text = brl.translate(xfinal_text)
+if data_float.loc[x_pos_max,0] < 0:
+    caract_resta = [['001001']]
+    for i in xfinal_text[0]:
+        caract_resta[0].append(i)
+    xfinal_text = caract_resta
+xfinal_text = brl.toUnicodeSymbols(xfinal_text, flatten=True)
+axbraille.set_xticks([data_float.loc[x_pos_min,0],data_float.loc[x_pos_middle,0],data_float.loc[x_pos_max,0]], [xinicio_text,xmedio_text,xfinal_text], fontsize=24)
+# 3 valores de eje y en braille
+# Found min, middle, max possitions and values
+abs_val_array = np.abs(data_float.loc[:,1] - data_float.loc[:,1].min())
+y_pos_min = abs_val_array.idxmin()
+middle = ((data_float.loc[:,1].max() - data_float.loc[:,1].min())/2) + data_float.loc[:,1].min()
+abs_val_array = np.abs(data_float.loc[:,1] - middle)
+y_pos_middle = abs_val_array.idxmin()
+abs_val_array = np.abs(data_float.loc[:,1] - data_float.loc[:,1].max())
+y_pos_max = abs_val_array.idxmin()
+
+y_pos_min_text = str(int(data_float.loc[y_pos_min,1]))
+y_pos_min_text = brl.translate(y_pos_min_text)
+if data_float.loc[y_pos_min,1] < 0:
+    caract_resta = [['001001']]
+    for i in y_pos_min_text[0]:
+        caract_resta[0].append(i)
+    y_pos_min_text = caract_resta
+y_pos_min_text = brl.toUnicodeSymbols(y_pos_min_text, flatten=True)
+y_pos_middle_text = str(int(data_float.loc[y_pos_middle,1]))
+y_pos_middle_text = brl.translate(y_pos_middle_text)
+if data_float.loc[y_pos_middle,1] < 0:
+    caract_resta = [['001001']]
+    for i in y_pos_middle_text[0]:
+        caract_resta[0].append(i)
+    y_pos_middle_text = caract_resta
+y_pos_middle_text = brl.toUnicodeSymbols(y_pos_middle_text, flatten=True)
+y_pos_max_text = str(int(data_float.loc[y_pos_max,1]))
+y_pos_max_text = brl.translate(y_pos_max_text)
+if data_float.loc[y_pos_max,1] < 0:
+    caract_resta = [['001001']]
+    for i in y_pos_max_text[0]:
+        caract_resta[0].append(i)
+    y_pos_max_text = caract_resta
+y_pos_max_text = brl.toUnicodeSymbols(y_pos_max_text, flatten=True)
+axbraille.set_yticks([data_float.loc[y_pos_min,1],data_float.loc[y_pos_middle,1],data_float.loc[y_pos_max,1]], [y_pos_min_text,y_pos_middle_text,y_pos_max_text], fontsize=24)
+
+axbraille.set_title(' ')
+x = brl.translate('x')
+x = brl.toUnicodeSymbols(x, flatten=True)
+axbraille.set_xlabel(x, fontsize=24, labelpad=10)
+y = brl.translate('y')
+y = brl.toUnicodeSymbols(y, flatten=True)
+axbraille.set_ylabel(y, fontsize=24, labelpad=10, rotation=0)
+axbraille.plot(data_float.loc[:, 0], data_float.loc[:, 1], '#2874a6', linewidth=3)
+# Ejes de coordenadas
+if data_float.loc[:, 0].min() < 0:
+    axbraille.axvline(x=0, color='k', linewidth=1)
+if data_float.loc[:, 1].min() < 0:
+    axbraille.axhline(y=0, color='k', linewidth=1)
+# Resize
+figbraille.tight_layout()
+# Save braille figure
+brailleplot_path = path[:-4] + 'plot-braille.png'
+figbraille.savefig(brailleplot_path)
+plt.close()
 
 # Reproduction
 # Normalize the data to sonify
@@ -137,8 +240,10 @@ ax_noise = plt.axes()
 ax_noise.plot(data_float.loc[:, 0], y1_noise, '#f39c12', linewidth=3)
 ax_noise.set_xlabel('x')
 ax_noise.set_ylabel('y')
-ax_noise.axhline(y=0, color='k', linewidth=1)
-ax_noise.axvline(x=0, color='k', linewidth=1)
+if data_float.loc[:, 0].min() < 0:
+    ax_noise.axvline(x=0, color='k', linewidth=1)
+if y1_noise.min() < 0:
+    ax_noise.axhline(y=0, color='k', linewidth=1)
 image_name = path[:-4] + 'plot_noise.png'
 fig_noise.savefig(image_name)
-
+plt.close()
